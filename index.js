65e35e6eaad79630c203ebc88da7868aeba0819e
Greg Fiorentino <gtfiorentino@gmail.com>
2018-10-20 21:08:43 -0400

    example of getting late train data

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const fetch = require('node-fetch');
 const TRAIN_VIEW_URL = 'http://www3.septa.org/hackathon/TrainView/';
 const SCHEDULE_URL = 'http://www3.septa.org/hackathon/RRSchedules/';
 
+const MY_TRAIN = '9467';
+const MY_STATION = 'Suburban Station';
+
 async function main() {
   try {
     // "Train View" returns top-level data about each train
@@ -11,10 +14,43 @@ async function main() {
     // Append schedule to train view for full train data
     const trainData = await getScheduleData(trainView);
 
-    console.log(JSON.stringify(trainData));
+    if (isTrainLate(MY_TRAIN, trainData)) {
+      console.log(getTrainStop(MY_TRAIN, trainData, MY_STATION));
+    } else {
+      console.log(`Train ${MY_TRAIN} is on time`);
+    }
   } catch (error) {
-    console.error(error);
+    console.log({ error });
   }
+}
+
+// getTrain finds train data by train number (trainno)
+function getTrain(trainno, trainData) {
+  const train = trainData.find(t => t.trainno === trainno);
+
+  if (!train) throw new Error(`Train ${trainno} not found`);
+
+  return train;
+}
+
+// isTrainLate returns true when train's late property is greater than zero
+function isTrainLate(trainno, trainData) {
+  const train = getTrain(trainno, trainData);
+
+  return train.late > 0;
+}
+
+// getTrainStop returns data for a specific station stop associated with
+// a train number (trainno)
+function getTrainStop(trainno, trainData, station) {
+  const train = getTrain(trainno, trainData);
+
+  const trainStop = train.trainStops.find(tS => tS.station === station)
+
+  return {
+    ...trainStop,
+    trainno,
+  };
 }
 
 // Append schedule data (including stops and times) 
